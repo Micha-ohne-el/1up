caee4baf44cb049c9bee7bad0386cf1e64be6d54
Micha Lehmann <michalehmann0112@gmail.com>
2022-08-06 14:07:44 +0200

    Add safety prompt to teardown task

--- teardown.ts
+++ teardown.ts
@@ -1,4 +1,17 @@
 import {sql} from '/db.ts';
+import {readLines} from '/deps/io.ts';
+
+console.warn('Are you sure? Please confirm by typing "Yes, drop all my tables, please." and then pressing Enter.');
+
+for await (const line of readLines(Deno.stdin)) {
+  if (line === 'Yes, drop all my tables, please.') {
+    console.log('Okay.');
+    break;
+  }
+
+  console.log('Aborting.');
+  Deno.exit();
+}
 
 console.log('Dropping table "xp"...');
 
