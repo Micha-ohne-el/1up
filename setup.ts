dcc5d6852c0a4546570642376bebede71162777a
Micha Lehmann <michalehmann0112@gmail.com>
2022-08-06 13:46:21 +0200

    Declare primary keys for database

--- setup.ts
+++ setup.ts
@@ -1,12 +1,12 @@
 import {sql} from '/db.ts';
 
-await sql
-  `
-    CREATE TABLE xp (
-      guildId bigint,
-      userId bigint,
-      xp real
-    )
-  `;
+await sql`
+  CREATE TABLE xp (
+    guildId bigint,
+    userId bigint,
+    xp real NOT NULL,
+    PRIMARY KEY(guildId, userId)
+  )
+`;
 
 Deno.exit();
