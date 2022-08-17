2a7b52405de69245b3968098e7a2c83601fc006f
Micha Lehmann <michalehmann0112@gmail.com>
2022-08-17 21:50:25 +0200

    Implement update command very crudely

---
+++ src/business/commands/update.ts
@@ -0,0 +1,43 @@
+import {MessageContext} from '/business/message-context.ts';
+import {command, Command, Response} from '/business/commands.ts';
+import {getOwnerId} from '/util/secrets.ts';
+import {error} from '/deps/log.ts';
+
+@command('update')
+class _Update extends Command {
+  override async invoke({authorId}: MessageContext): Promise<Response> {
+    if (authorId !== getOwnerId()) {
+      return {success: false};
+    }
+
+    const fetchErrors = await Deno.run({
+      cmd: ['git', 'fetch', '--all'],
+      stderr: 'piped',
+      clearEnv: true
+    }).stderrOutput();
+
+    if (fetchErrors.length > 0) {
+      error(new TextDecoder().decode(fetchErrors));
+    }
+
+    const resetErrors = await Deno.run({
+      cmd: ['git', 'reset', '--hard'],
+      stderr: 'piped',
+      clearEnv: true
+    }).stderrOutput();
+
+    if (resetErrors.length > 0) {
+      error(new TextDecoder().decode(resetErrors));
+    }
+
+    if (fetchErrors.length > 0 || resetErrors.length > 0) {
+      return {success: false};
+    }
+
+    await Deno.run({
+      cmd: ['deno', 'task', 'run']
+    });
+
+    Deno.exit();
+  }
+}
--- src/business/handle-command.ts
+++ src/business/handle-command.ts
@@ -8,6 +8,7 @@ import '/business/commands/role.ts';
 import '/business/commands/level.ts';
 import '/business/commands/logs.ts';
 import '/business/commands/status.ts';
+import '/business/commands/update.ts';
 
 export async function handleCommand(text: string, context: MessageContext): Promise<Response | void> {
   const possibleCommands = getPossibleCommands(commands, text);
