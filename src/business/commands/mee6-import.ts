d5b70530a966a6a43e7ba6e3238279432a481385
Micha Lehmann <michalehmann0112@gmail.com>
2022-08-27 20:47:50 +0200

    Write rest of MEE6 import command
    The code is definitely not final, as it will be refactored to be a lot cleaner.

--- src/business/commands/mee6-import.ts
+++ src/business/commands/mee6-import.ts
@@ -3,7 +3,7 @@ import {command, availableTo, ServerOwner, Command, param, Text, Response} from 
 import {sql} from '/data/db.ts';
 import {error, warning, info} from '/util/log.ts';
 import postgres from 'https://deno.land/x/postgresjs@v3.2.4/types/index.d.ts';
-import {getLevelFromXp} from '../../data/xp.ts';
+import {getLevelFromXp} from '/data/xp.ts';
 
 @command('mee6import')
 @availableTo(ServerOwner)
@@ -12,37 +12,64 @@ class _Mee6Import extends Command {
   things!: string;
 
   override async invoke({guildId}: MessageContext): Promise<Response> {
+    const pageSize = 1000;
+
     if (!guildId) {
       throw new Error('This command must be used on the server you are trying to import to.');
     }
 
     const things: Things = {
-      messages: this.things.toLowerCase().includes('messages'),
-      xp: this.things.toLowerCase().includes('xp'),
+      stats: this.things.toLowerCase().includes('stats'),
       range: this.things.toLowerCase().includes('range')
     };
 
-    await sql.begin(async (sql) => {
-      for (let page = 0; true; page++) {
-        const response =
-          await fetch(`https://mee6.xyz/api/plugins/levels/leaderboard/${guildId}?limit=${this.pageSize}&page=${page}`);
+    info('Starting import process...');
+    const result = await sql.begin(async (sql) => {
+      const data: Partial<Data> = {};
 
-        if (!response.ok) {
-          error('MEE6 API Response was not OK. Rolling back.', {response, text: await response.text()});
-          return await sql`ROLLBACK`;
+      for (let page = 0; true; page++) {
+        info(`Fetching page #${page}`);
+
+        const httpResponse =
+          await fetch(`https://mee6.xyz/api/plugins/levels/leaderboard/${guildId}?limit=${pageSize}&page=${page}`);
+
+        if (httpResponse.status === 404) {
+          error('Could not find MEE6 leaderboard for this server.');
+          await sql`ROLLBACK`;
+          return false;
         }
 
-        const data = await response.json() as Mee6Response;
-        const entries = mee6ResponseToEntries(data);
+        if (!httpResponse.ok) {
+          error('MEE6 API Response was not OK. Rolling back.', {httpResponse, text: await httpResponse.text()});
+          await sql`ROLLBACK`;
+          return false;
+        }
 
-        if (entries.length > this.pageSize) {
-          warning(`MEE6 API sent back too many entries. Continuing.`, {expected: this.pageSize, got: entries.length});
+        const response = await httpResponse.json() as Mee6Response;
+        const range = mee6ResponseToRange(response);
+        const entries = mee6ResponseToEntries(response);
+
+        if (entries.length > pageSize) {
+          warning(`MEE6 API sent back more entries than expected. Continuing.`, {expected: this.pageSize, got: entries.length});
+        }
+
+        if (!isRange(range)) {
+          error('Range is invalid. Rolling back.', {response});
+          await sql`ROLLBACK`;
+          return false;
         }
 
         for (const entry of entries) {
           if (!isEntry(entry)) {
-            error('At least one entry is invalid. Rolling back.', {entry, entries, data});
-            return await sql`ROLLBACK`;
+            error('At least one entry is invalid. Rolling back.', {entry, entries, response});
+            await sql`ROLLBACK`;
+            return false;
+          }
+
+          if (entry.guildId !== guildId) {
+            error('At least one entry is for the wrong guildId. Rolling back.', {entry, entries, response});
+            await sql`ROLLBACK`;
+            return false;
           }
 
           if (entry.level !== getLevelFromXp(entry.xp)) {
@@ -53,59 +80,128 @@ class _Mee6Import extends Command {
           }
         }
 
-        await this.importData(sql, entries, things);
+        data.range = range;
+        if (!data.entries) {
+          data.entries = [];
+        }
+        data.entries.push(...entries);
+        data.guildId = mee6ResponseToGuildId(response);
 
-        if (entries.length < this.pageSize) {
+        if (entries.length < pageSize) {
           info('Reached last page.', page);
-          return;
+          break;
         }
       }
+
+      await this.importData(sql, data as Data, things);
+
+      return true;
     });
 
-    return {
-      success: true,
-      message: 'Successfully imported data from MEE6!'
-    };
+
+    if (result) {
+      return {
+        success: true,
+        message: 'Successfully imported data from MEE6!'
+      };
+    } else {
+      return {
+        success: false,
+        message: 'An error occurred, sorry.'
+      }
+    }
   }
 
-  private async importData(sql: postgres.TransactionSql<any>, entries: Entry[], things: Things) {
-    if (things.range) {
-      // clear table range
-      // insert all the new values (probably should make use of postgres.js's cool features for this)
+  private async importData(sql: postgres.TransactionSql<any>, {guildId, range, entries}: Data, things: Things) {
+    if (things.range && range) {
+      await this.importRange(sql, guildId, range);
+
       info('Successfully imported range!');
     } else {
-      info('Did not import rang, because user did not request it.', things);
+      info('Did not import range.', {range, things});
     }
+    if (things.stats && entries) {
+      await this.importStats(sql, guildId, entries);
 
-    if (things.xp) {
-      // same as above
-      info('Successfully imported xp!');
+      info('Successfully imported stats!');
     } else {
-      info('Did not import xp, because user did not request it.', things);
-    }
-
-    if (things.messages) {
-      // yada yada yada
-      info('Successfully imported messages!');
-    } else {
-      info('Did not import messages, because user did not request it.', things);
+      info('Did not import stats.', {entries, things});
     }
   }
 
-  private readonly pageSize = 1000;
+  private async importRange(sql: postgres.TransactionSql<any>, guildId: bigint, range: [number, number]) {
+    info(`Deleting all rows in table "range" with guildId = ${guildId}`);
+    await sql`
+      DELETE FROM range
+      WHERE guildId = ${guildId.toString()}
+    `;
+    info('Success.');
+
+    info('Inserting into table "range".', {range});
+    await sql`
+      INSERT INTO range (
+        guildId, first, last
+      )
+      VALUES (
+        ${guildId.toString()}, ${range[0]}, ${range[1]}
+      )
+    `;
+    info('Success.');
+  }
+
+  private async importStats(sql: postgres.TransactionSql<any>, guildId: bigint, entries: Entry[]) {
+    info(`Deleting all rows in table "stats" with guildId = ${guildId}`);
+    await sql`
+      DELETE FROM stats
+      WHERE guildId = ${guildId.toString()}
+    `;
+    info('Success.');
+
+    info('Inserting into table "stats".', {entries});
+
+    for (const {guildId, userId, xp, messages} of entries) {
+      await sql`
+        INSERT INTO stats (
+          guildId, userId, xp, messages
+        )
+        VALUES (
+          ${guildId.toString()}, ${userId.toString()}, ${xp}, ${messages}
+        )
+      `;
+    }
+    info('Success.');
+  }
+}
+
+function mee6ResponseToRange(response: Mee6Response): [number, number] {
+  return response.xp_per_message;
 }
 
 function mee6ResponseToEntries(response: Mee6Response): Entry[] {
   return response.players.map(mee6PlayerToEntry);
 }
 
+function mee6ResponseToGuildId(response: Mee6Response): bigint {
+  return BigInt(response.guild.id);
+}
+
 function mee6PlayerToEntry(player: Mee6Player): Entry {
   return {
     userId: BigInt(player.id),
+    guildId: BigInt(player.guild_id),
     level: player.level,
     xp: player.xp,
     messages: player.message_count
   }
+}
+
+function isRange(value: unknown): value is [number, number] {
+  return typeof value === 'object'
+    && value !== null
+    && value instanceof Array
+    && value.length === 2
+    && typeof value[0] === 'number'
+    && typeof value[1] === 'number';
 }
 
 function isEntry(value: unknown): value is Entry {
@@ -114,32 +210,42 @@ function isEntry(value: unknown): value is Entry {
     && typeof (value as Partial<Entry>).userId === 'bigint'
     && typeof (value as Partial<Entry>).level === 'number'
     && typeof (value as Partial<Entry>).xp === 'number'
-    && typeof (value as Partial<Entry>).messages === 'number'
+    && typeof (value as Partial<Entry>).messages === 'number';
 }
 
 interface Things {
-  xp: boolean;
-  messages: boolean;
+  stats: boolean;
   range: boolean;
 }
 
-// The following interfaces intentionally only include fields that we care about.
+interface Data {
+  range?: [number, number];
+  entries?: Entry[];
+  guildId: bigint;
+}
 
 interface Entry {
   userId: bigint;
+  guildId: bigint;
   level: number;
   xp: number;
   messages: number;
 }
 
+// The following interfaces intentionally only include fields that we care about.
+
 interface Mee6Response {
   players: Mee6Player[];
   xp_per_message: [number, number];
+  guild: {
+    id: string; // bigint
+  };
 }
 
 interface Mee6Player {
   detailed_xp: [number, number, number];
   id: string; // bigint
+  guild_id: string; // bigint
   level: number;
   message_count: number;
   xp: number;
